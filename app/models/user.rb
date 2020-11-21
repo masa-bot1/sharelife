9b12e889d99acf1d396974dd9dda23cd042d1068
masa-bot1 <bdyht515@yahoo.co.jp>
2020-11-21 11:39:19 +0900

    新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, uniqueness: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, format: { with: PASSWORD_REGEX }
+  with_options presence: true do
+    validates :password_confirmation
+    validates :nickname
+    validates :name,         format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :name_reading, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birthday
+  end
 end
