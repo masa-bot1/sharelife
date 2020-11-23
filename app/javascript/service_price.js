0af1801d0620ddfcbac0d95df958fed723383000
masa-bot1 <bdyht515@yahoo.co.jp>
2020-11-23 15:38:16 +0900

    value属性の取得

--- app/javascript/service_price.js
+++ app/javascript/service_price.js
@@ -1,7 +1,8 @@
 function post (){
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
-    console.log("イベント発火");
+    const inputValue = priceInput.value;
+    console.log(inputValue);
   });
 }
 
