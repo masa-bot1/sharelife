7be0e3c92f9b2680f3b66ad61604ae304cc9c1b7
masa-bot1 <bdyht515@yahoo.co.jp>
2020-11-23 15:27:46 +0900

    イベント発火の実装

--- app/javascript/service_price.js
+++ app/javascript/service_price.js
@@ -1,5 +1,8 @@
 function post (){
-  console.log("OK");
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    console.log("イベント発火");
+  });
 }
 
 window.addEventListener('load', post);
