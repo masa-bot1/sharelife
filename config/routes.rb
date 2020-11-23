0e9347cbbcbd75766cafc5a2a25c25fb1bd5cdce
masa-bot1 <bdyht515@yahoo.co.jp>
2020-11-23 18:50:49 +0900

    詳細画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'services#index'
-  resources :services, only: [:index, :new, :create]
+  resources :services, only: [:index, :new, :create, :show]
 end
