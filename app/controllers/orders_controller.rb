09af3561711232cf3f20ed731fdc5adefac87526
masa-bot1 <bdyht515@yahoo.co.jp>
2020-11-24 12:40:03 +0900

    createアクションの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,4 +4,21 @@ class OrdersController < ApplicationController
     @user_order = UserOrder.new
     @service = Service.find(params[:service_id])
   end
+
+  def create
+    @user_order = UserOrder.new(order_params)
+    @service = Service.find(params[:service_id])
+    if @user_order.valid?
+      @user_order.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:user_order).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, service_id: params[:service_id])
+  end
 end
--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -1,13 +1,12 @@
 class UserOrder
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :service_id, :order_id, :token
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :service_id, :order_id
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :city
     validates :house_number
     validates :phone_number, format: { with: /\A\d{11}\z/ }
-    validates :token
   end
   validates :prefecture_id, numericality: { other_than: 0 }
 
