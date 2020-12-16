0dcfff67c030013d699c0964f3c9ecd39fd6ede3
masa-bot1 <bdyht515@yahoo.co.jp>
2020-12-16 14:30:54 +0900

    不要なアクションの削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,19 +8,5 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect_to "show"
-    else
-      render :edit
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, :category_id) # 編集出来る情報を制限
-  end
+  
 end
