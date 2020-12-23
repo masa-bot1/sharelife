5ca3915f03a6d6f94e7566a61d20db52424a76c6
masa-bot1 <bdyht515@yahoo.co.jp>
2020-12-23 11:23:30 +0900

    不要なアクションの削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,9 @@
 class UsersController < ApplicationController
+  
   def show
     @user = User.find(params[:id])
     @services = @user.services
     @orders = Order.where(service_id: @services)
-  end
-
-  def edit
-    @user = User.find(params[:id])
   end
   
 end
