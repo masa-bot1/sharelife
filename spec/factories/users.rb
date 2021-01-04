ca8172fec88c8951eb39da79454538dfb95f09ac
masa-bot1 <bdyht515@yahoo.co.jp>
2021-01-05 02:20:55 +0900

    ゲストログイン機能の修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { 'tarou' }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { 'password1' }
     password_confirmation { password }
     last_name             { '山田' }
     first_name            { '太郎' }
