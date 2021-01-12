fa1485f21ab323aa4d966252b27263d7634fd8ac
masa-bot1 <bdyht515@yahoo.co.jp>
2021-01-13 00:32:56 +0900

    ドロップダウンリストの重複部分調整

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,9 +1,10 @@
 class ServicesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_service, only: [:show, :edit, :update, :destroy]
+  before_action :set_categories, only: [:index, :show, :search, :select_category_index]
 
   def index
     @services = Service.includes(:user).order('created_at DESC').limit(10)
-    @categories = Category.all.drop(1)
     @users = User.all.order('created_at DESC').limit(10)
   end
 
@@ -21,7 +22,6 @@ class ServicesController < ApplicationController
   end
 
   def show
-    @service = Service.find(params[:id])
     @user = @service.user
     @services = @user.services
     @orders = Order.where(service_id: @services)
@@ -29,12 +29,10 @@ class ServicesController < ApplicationController
   end
 
   def edit
-    @service = Service.find(params[:id])
     redirect_to action: :index unless current_user.id == @service.user_id
   end
 
   def update
-    @service = Service.find(params[:id])
     if @service.update(service_params)
       redirect_to service_path(@service.id), method: :get
     else
@@ -43,7 +41,6 @@ class ServicesController < ApplicationController
   end
 
   def destroy
-    @service = Service.find(params[:id])
     redirect_to root_path unless current_user.id == @service.user_id
     redirect_to root_path if @service.destroy
   end
@@ -79,4 +76,11 @@ class ServicesController < ApplicationController
     params.require(:service).permit(:name, :description, :category_id, :transportation_id, :prefecture_id, :duration_id, :price, :image).merge(user_id: current_user.id)
   end
 
+  def set_service
+    @service = Service.find(params[:id])
+  end
+
+  def set_categories
+    @categories = Category.all.drop(1)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,19 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit]
 
   def show
-    @user = User.find(params[:id])
     @services = @user.services
     @orders = Order.where(service_id: @services)
+    @categories = Category.all.drop(1)
   end
 
   def edit
-    @user = User.find(params[:id])
     redirect_to root_path unless current_user.id == @user.id
   end
-  
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
